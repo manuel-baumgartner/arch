b69788399ef1be60f8bf00eda81089216088381b
Manuel Baumgartner <manuel.baumgartner@student.hs-rm.de>
2017-02-27 17:00:23 +0100

    short update, but still not finished

--- install_pi.sh
+++ install_pi.sh
@@ -12,7 +12,7 @@
 	OWNER="manu" # This is the user who has local folder rights
 
 function main {
-	local IN
+	
 	echo "-- Raspberry Pi setup --"
 	echo "- Configurations -"
 
@@ -23,48 +23,48 @@ function main {
 	echo "Permanent mount: $PI_MOUNT_PERM" 
 	echo "Change? (yes/No)" 
 
-	read $IN
+	read IN 
 
-	if [ "$IN" == 'yes' ] ; then
+	if [[ $IN =~ ^[Yy]*$ ]] ; then
 		setVars	
 	fi
 	
 	mountPi
 }
 
+
 function setVars
 {
-	local IN = '0'
-	
 		echo "1: pi ip"
 		echo "2: pi mount path"
 		echo "3: pi user"
 		echo "4: pi mount type"
 		echo "5: pi mount permanent"
-		
+		read IN
+
 		if [ $IN -eq 1 ] ; then
 			echo "change $PI_IP: "
-			read $PI_IP
+			read PI_IP
 		fi
 
 		if [ $IN -eq 2 ] ; then
 			echo "change $PI_PATH: "
-			read $PI_PATH
+			read PI_PATH
 		fi
 
-		if [ $IN -eq 3 } ; then
+		if [ $IN -eq 3 ] ; then
 			echo "change $PI_USER: "
-			read $PI_USER
+			read PI_USER
 		fi
 
 		if [ $IN -eq 4 ] ; then
 			echo "change $PI_MOUNT_TYPE: "
-			read $PI_MOUNT_TYPE
+			read PI_MOUNT_TYPE
 		fi
 
 		if [ $IN -eq 5 ] ; then
 			echo "change $PI_MOUNT_PERM: "
-			read $PI_MOUNT_PERM
+			read PI_MOUNT_PERM
 		fi
 }
 
@@ -72,7 +72,17 @@ function setVars
 # always check if mount is possible before putting it to fstab
 function mountPi
 {
-	sudo mkdir $PI_PATH
+	echo "trying to create a folder: $PI_PATH"
+	sudo mkdir  $PI_PATH
+	if [ $? -ne 0 ] ; then
+		echo "Do you want to continue? This will change the folder settings!! (Yes/no)"
+		read IN
+		if [[ ! $IN =~ ^[Yy$ ]]  ; then
+			exit
+		fi
+	fi
+
+	echo "trying to change folder owner to: $OWNER"
 	sudo chown $OWNER:users $PI_PATH
 
 	
@@ -94,7 +104,13 @@ function mountPi
 			echo "Device exists and was mounted"
 		else
 			echo "Device not found"
-			#exit
+			echo "Do you want to change the settings? (Yes/no)"
+			read IN
+			if [[ $IN =~ ^[Yy]$ ]] ; then
+				setVars
+			fi
+
+			exit
 		fi
 
 		if [ $PI_MOUNT_PERM -eq 1 ] ; then
@@ -107,7 +123,12 @@ function mountPi
 
 	if [ $PI_MOUNT_TYPE -eq 3 ] ; then
 		sudo mount -t cifs -o user=$PI_USER $PI_IP:/ $PI_PATH
-
+		if [ $? -eq 0 ] ; then
+			echo "Device exists and was mounted"
+		else
+			echo "Device not found"
+			exit
+		fi
 
 
 
